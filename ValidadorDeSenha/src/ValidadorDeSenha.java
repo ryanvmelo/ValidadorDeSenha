a3c155a209b08e6ebbdb2a751ff87f3354bb7f13
Ryan Melo <68710321+ryanvmelo@users.noreply.github.com>
2022-12-28 01:16:29 -0300

    Versão revisada
    Diminuição do código

--- ValidadorDeSenha/src/ValidadorDeSenha.java
+++ ValidadorDeSenha/src/ValidadorDeSenha.java
@@ -5,23 +5,18 @@ public class ValidadorDeSenha {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int senha = 2022;
-		System.out.println("Digite a senha:");
-		senha = sc.nextInt();
-		if (senha == 2022) {
-			System.out.println("Acesso garantido!");
-		}else{
-			System.out.println("Acesso negado!");
-		}
-		while (senha != 2022) {
-		senha = sc.nextInt();
-		if (senha == 2022) {
-			System.out.println("Acesso garantido!");
-		}else{
-			System.out.println("Acesso negado");
-		}
-		}
+		int senha;
 		
+		System.out.print("Digite a senha:");
+		senha = sc.nextInt();
+		
+		do {
+			System.out.println("Acesso negado!");
+			System.out.print("Digite novamente: ");
+			senha = sc.nextInt();
+		}while (senha != 2022);
+		
+		System.out.println("Acesso garantido");
 	}
 
 }
